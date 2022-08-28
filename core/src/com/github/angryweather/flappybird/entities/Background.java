87bd20489371a698571f1a6661a8feac55fa6a21
Weronika <WerRic96@protonmail.com>
2022-08-28 23:27:25 +0200

    add more Background variables

--- core/src/com/github/angryweather/flappybird/entities/Background.java
+++ core/src/com/github/angryweather/flappybird/entities/Background.java
@@ -2,6 +2,9 @@ package com.github.angryweather.flappybird.entities;
 
 public class Background {
     public static final String BACKGROUND_IMAGE = "assets/background.png";
-    public int backgroundScroll = 0;
+    public float backgroundScroll = 0f;
+    public static final int BACKGROUND_SPEED = 60;
+    public static final int BACKGROUND_LOOPING_POINT = 413;
+
 
 }
