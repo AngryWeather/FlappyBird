774c01ccd941b29a76eb6a540b6504cff15b2a56
Weronika <WerRic96@protonmail.com>
2022-08-28 23:30:45 +0200

    make backgroundScroll static

--- core/src/com/github/angryweather/flappybird/entities/Background.java
+++ core/src/com/github/angryweather/flappybird/entities/Background.java
@@ -2,7 +2,7 @@ package com.github.angryweather.flappybird.entities;
 
 public class Background {
     public static final String BACKGROUND_IMAGE = "assets/background.png";
-    public float backgroundScroll = 0f;
+    public static float backgroundScroll = 0f;
     public static final int BACKGROUND_SPEED = 60;
     public static final int BACKGROUND_LOOPING_POINT = 413;
 
