467edf8102e4fe973070e4f460a680236392d3e5
Weronika <WerRic96@protonmail.com>
2022-08-28 13:00:06 +0200

    changed dimensions of the screen

--- desktop/src/com/github/angryweather/flappybird/DesktopLauncher.java
+++ desktop/src/com/github/angryweather/flappybird/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("FlappyBird");
-		config.setWindowedMode(1157, 288);
+		config.setWindowedMode(1280, 720);
 		new Lwjgl3Application(new FlappyBird(), config);
 	}
 }
