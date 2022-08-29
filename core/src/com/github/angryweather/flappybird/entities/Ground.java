b32be89e8ea70ffc6ce054723113c656e1d5152f
Weronika <WerRic96@protonmail.com>
2022-08-29 23:09:25 +0200

    add Ground data

--- core/src/com/github/angryweather/flappybird/entities/Ground.java
+++ core/src/com/github/angryweather/flappybird/entities/Ground.java
@@ -2,4 +2,7 @@ package com.github.angryweather.flappybird.entities;
 
 public class Ground {
     public static final String GROUND_IMAGE = "assets/ground.png";
+    public float groundScroll = 0f;
+    public static final int GROUND_SPEED = 30;
+    public static final int GROUND_LOOPING_POINT = 60;
 }
